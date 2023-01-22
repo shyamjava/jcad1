90835ac8598a87e809117d83d4d6161e0b09f3f6
Shyam Basani (TMS) <shyam.basani@toyota.com>
2023-01-22 15:04:43 -0600

    Updated 1

--- DataTypes.java
+++ DataTypes.java
@@ -1,8 +1,26 @@
-public class DataTypes {
+public class DataTypes extends HelloWorld{
 
 	public static void main(String s[]){
 	
 		System.out.println("Data Types in Java");
+		
+		byte b = 123;
+		System.out.println(b);
+		
+		System.out.println(b);
+		
+		System.out.println(Byte.MIN_VALUE+"-"+Byte.MAX_VALUE);
+		
+		System.out.println(Short.MIN_VALUE+"-"+Short.MAX_VALUE);
+		
+		System.out.println(Integer.MIN_VALUE+"-"+Integer.MAX_VALUE);
+		char c = '\u0C32';
+		System.out.println(c);
+		int x = 100;
+		Integer y = 200;
+		Integer z = x + y;
+		System.out.println(z);
+		
 		
 	}
 
